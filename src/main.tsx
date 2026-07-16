627e579c8a566316ee64ee018ee1f9660101acdc
yedlachandana4 <yedlachandana4@gmail.com>
2026-07-16 18:29:18 +0530

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+
+import App from "./App";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnHover
+          theme="light"
+        />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
